84b71ae81fdd3cbecec332eaaee6cc69fc5c833e
goatl <goatlegflex@gmail.com>
2022-05-04 21:34:11 +0300

    eslint fix 2

--- notes/.eslintrc.js
+++ notes/.eslintrc.js
@@ -1,20 +1,20 @@
-// const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules)
-//   .reduce((acc, rule) => { acc[`jsx-a11y/${rule}`] = 'off'; return acc }, {})
-//
-// module.exports = {
-//   "extends": ["react-app", "airbnb", "plugin:jsx-a11y/recommended", "prettier"],
-//   "plugins": ["prettier"],
-//   "rules": {
-//     ...a11yOff,
-//     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-//     "react/function-component-definition": 0,
-//     "import/prefer-default-export": 0,
-//   },
-//   "settings": {
-//     "import/resolver": {
-//       "node": {
-//         "moduleDirectory": ["node_modules", "src/"]
-//     }
-//     }
-//   }
-// }
+const a11yOff = Object.keys(require('eslint-plugin-jsx-a11y').rules)
+  .reduce((acc, rule) => { acc[`jsx-a11y/${rule}`] = 'off'; return acc }, {})
+
+module.exports = {
+  "extends": ["react-app", "airbnb", "plugin:jsx-a11y/recommended", "prettier"],
+  "plugins": ["prettier"],
+  "rules": {
+    ...a11yOff,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/function-component-definition": 0,
+    "import/prefer-default-export": 0,
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": ["node_modules", "src/"]
+    }
+    }
+  }
+}
